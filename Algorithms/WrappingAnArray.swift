fbb56dea8bec526e03499e44c2f83e637e368440
Darragh Blake <darragh.blake48@gmail.com>
2017-11-02 16:44:06 +0000

    Wrapping An Array

---
+++ Algorithms/WrappingAnArray.swift
@@ -0,0 +1,88 @@
+//
+//  WrappingAnArray.swift
+//  Algorithms
+//
+//  Created by Darragh on 11/2/17.
+//  Copyright © 2017 Darragh. All rights reserved.
+//
+
+import Foundation
+
+// Assume we have a list of tracks
+let tracks = ["a", "b", "c", "d", "e"]
+
+// Algorithm should return this playlist if we select track "d"
+["d", "e", "a" ,"b" ,"c"]
+
+let selectedTrack = "d"
+var playlist = [String]()
+var priorTracks = [String]()
+
+for track in tracks {
+    if track == selectedTrack || playlist.count > 0 {
+        playlist.append(track)
+    } else {
+        priorTracks.append(track)
+    }
+    
+}
+
+playlist + priorTracks
+
+// Alternative way to return the same result
+let index = tracks.index(where: {return $0 == selectedTrack})
+let prefixArray = tracks.prefix(upTo: index!)
+let suffixArray = tracks.suffix(from: index!)
+
+let arr = suffixArray + prefixArray
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
