066e48ae1dcf544fa608d757a16f28f524fe7136
Darragh Blake <darragh@Rutas-MacBook-Pro.local>
2017-12-15 13:34:29 +0000

    HigherOrderFunctions setup

---
+++ Algorithms/HigherOrderFunctions.swift
@@ -0,0 +1,28 @@
+//
+//  HigherOrderFunctions.swift
+//  Algorithms
+//
+//  Created by Darragh Blake on 15/12/2017.
+//  Copyright © 2017 Darragh. All rights reserved.
+//
+
+import Foundation
+
+let numbers = [1, 2, 3, 4, 3, 3]
+
+// 1. filter int [3 ,3 ,3], then filter into [2, 4] (even numbers)
+
+
+// 2. transform [1, 2, 3, 4] -> [2, 4, 6, 8] using map
+
+
+// 3. sum [1, 2, 3, 4] -> 10 using reduce
+
+
+
+
+
+
+
+
+
